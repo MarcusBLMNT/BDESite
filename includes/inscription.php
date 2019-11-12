4b6262de0d548f8a207042cc1a922ace0d08b925
Marcus BLMNT <marcus.belmont@viacesi.fr>
2019-11-12 21:41:41 +0100

    premier commit

---
+++ includes/inscription.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title Connexion></title>
+    <link href="../public/css/inscrp.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+</head>
+<header>
+<?php
+    include('../includes/headerOff.html');
+    include('../includes/menu.php');
+    
+    ?>
+    </footer>
+</header>
+
+<body>
+   
+    <div class="inscrip">
+        <form>
+            <h2 style="color: white">Inscription</h2>
+            <input type="text" name="Nom d'utilisateur" placeholder="Nom"><br><br>
+            <input type="text" name="Nom d'utilisateur" placeholder="Prénom"><br><br>
+            <input type="text" name="Nom d'utilisateur" placeholder="Nom d'utilisateur"><br><br>
+            <input type="text" name="email" placeholder="adresse mail"> <br><br>
+            <input type="text" name="Mot de passe" placeholder="Mot de passe"><br><br>
+            <input type="text" name="Mot de passe" placeholder="Mot de passe"><br><br>
+            <a href=""><input type="button" value=" M'inscrire"></a>
+             <br><br>
+             
+           
+        </form>
+    </div>
+   
+</body><br><br><br><br>
+<footer>
+    <?php
+include('../includes/footer.html'); 
+?>
+</footer>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+</head>
+<?php
+
+include('../includes/headerOn.html'); ?>
+
+<div class="row">
+    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2" style="margin-left:0">
+        <?php
+        include('../includes/menu.php');
+        ?>
+    </div>
+    <div class="">
+        <?php
+        //inserez vos includes
+        ?>
+
+    </div>
+
+
+
+
+</div>
+
+<?php
+include('../includes/footer.html');
+
+
+
+
+
+?>
+
+
+</html>
