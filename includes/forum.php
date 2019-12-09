77efc7ad54ebafb0bf63a488136745943c4bf562
Marcus BLMNT <marcus.belmont@viacesi.fr>
2019-12-09 21:38:13 +0100

    emails des bde pris dans la base de donnée et insérés dans mailto

--- includes/forum.php
+++ includes/forum.php
@@ -33,8 +33,27 @@ $statut = getStatut();
         ?>
         <?php
         if ($statut == 3) {
+            $reqMailsBde = $bdd->prepare("SELECT email from utilisateur where utilisateur.id_Role=2");
+            $reqMailsBde->execute();
+            $reqMailsBde = $reqMailsBde->fetchAll(PDO::FETCH_CLASS);
+            $reqMailsBde = objectToArray($reqMailsBde);
+            $mailsBDE = "";
+
+            foreach ($reqMailsBde as  $key => $Mail) {
+                if ($key > 0) {
+                    $mailsBDE = $mailsBDE . ',';
+                }
+                $mailsBDE = $mailsBDE . $Mail['email'];
+            }
+
+
+
+
+
+
             ?>
-            <a href="mailto:truc@aremplacer?subject=Attention!%20Commentaires%20ou%20sujets%20déplacés%20sur%20le%20site%20du%20BDE
+
+            <a href="mailto:<?php echo ($mailsBDE); ?>?subject=Attention!%20Commentaires%20ou%20sujets%20déplacés%20sur%20le%20site%20du%20BDE
             &body=Voici les différents sujets déplacés :%0D%0A - nomSujet %0D%0AVoici les différents messages déplacés %0D%0A- corpsMessage du sujet nomSujet%0D%0Aveuillez agir en conséquences svp Merci!">notifier membres du bde</a>
         <?php
         }
